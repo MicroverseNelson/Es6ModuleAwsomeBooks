756848512a8cb75302b1b2b5a22811f4abddd076
MicroverseNelson <poetpenton@gmail.com>
2022-07-04 13:53:51 +0300

    wfixed modules and luxon

--- modules/BookStore.js
+++
@@ -1,7 +0,0 @@
-export default class BookStore {
-    constructor() {
-      this.booksArray = JSON.parse(localStorage.getItem('books'))
-        ? JSON.parse(localStorage.getItem('books'))
-        : [];
-    }
-  }
